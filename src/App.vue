883727a8fda25ae63527bca9fe10a77b44fb7b1e
Kim1am <ykimlam@163.com>
2021-07-09 10:55:29 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="jsx">
+import HelloWorld from 'comps/HelloWorld.vue'
+import logo from './assets/logo.png'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  render: () => {
+    ;<>
+      <img alt='Vue Logo' src={logo} />
+      <HelloWorld msg='Hello Vue3 + vite'></HelloWorld>
+    </>
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
